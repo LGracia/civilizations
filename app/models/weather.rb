1dfa0c19795ff01c024b81cf9325ce2d64c6e9c3
Leonel Gracia <leonel.gracia89@gmail.com>
2020-06-19 23:48:46 -0500

    refactoring logic in services

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,3 +1,3 @@
 class Weather < ApplicationRecord
-  enum weather_type: { rainy: 'lluvioso', drought: 'sequia' }
+  enum weather_type: { lluvioso: 'lluvioso', sequia: 'sequia', optimo: 'optimo' }
 end
---
+++ app/services/triangle_service.rb
@@ -0,0 +1,5 @@
+class TriangleService
+  class << self
+    
+  end
+end
